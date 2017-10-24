894d62aaefdc8f81748823671768338c8d992f9c
ThomasJones69 <tjones@ziggo.nl>
2017-10-24 14:34:56 +0200

    De array opdacht

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php
+   
+$week = array ("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag");
+
+for($counter=0; $counter < count($week); $counter++){
+    echo $week[$counter];
+    echo "<br>";
+}   
+?>
